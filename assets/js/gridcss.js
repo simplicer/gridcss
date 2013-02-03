f30ed03e9f66539fb72fa362e294af5290101025
Antonio Villamarin <ant@onio.org>
2013-02-03 16:24:16 +0100

    Cambios js

--- assets/js/gridcss.js
+++ assets/js/gridcss.js
@@ -4,6 +4,73 @@
 // JQuery Elastic by Jan Jarfalk <http://www.unwrongest.com> : MIT
 (function(e){jQuery.fn.extend({elastic:function(){var t=["paddingTop","paddingRight","paddingBottom","paddingLeft","fontSize","lineHeight","fontFamily","width","fontWeight","border-top-width","border-right-width","border-bottom-width","border-left-width","borderTopStyle","borderTopColor","borderRightStyle","borderRightColor","borderBottomStyle","borderBottomColor","borderLeftStyle","borderLeftColor"];return this.each(function(){function f(){var e=Math.floor(parseInt(n.width(),10));if(r.width()!==e){r.css({width:e+"px"});c(true)}}function l(e,t){var r=Math.floor(parseInt(e,10));if(n.height()!==r){n.css({height:r+"px",overflow:t})}}function c(e){var t=n.val().replace(/&/g,"&").replace(/ {2}/g," ").replace(/<|>/g,">").replace(/\n/g,"<br />");var u=r.html().replace(/<br>/ig,"<br />");if(e||t+" "!==u){r.html(t+" ");if(Math.abs(r.height()+i-n.height())>3){var a=r.height()+i;if(a>=o){l(o,"auto")}else if(a<=s){l(s,"hidden")}else{l(a,"hidden")}}}}if(this.type!=="textarea"){return false}var n=jQuery(this),r=jQuery("<div />").css({position:"absolute",display:"none","word-wrap":"break-word","white-space":"pre-wrap"}),i=parseInt(n.css("line-height"),10)||parseInt(n.css("font-size"),"10"),s=parseInt(n.css("height"),10)||i*3,o=parseInt(n.css("max-height"),10)||Number.MAX_VALUE,u=0;if(o<0){o=Number.MAX_VALUE}r.appendTo(n.parent());var a=t.length;while(a--){r.css(t[a].toString(),n.css(t[a].toString()))}n.css({overflow:"hidden"});n.bind("keyup change cut paste",function(){c()});e(window).bind("resize",f);n.bind("resize",f);n.bind("update",c);n.bind("blur",function(){if(r.height()<o){if(r.height()>s){n.height(r.height())}else{n.height(s)}}});n.bind("input paste",function(e){setTimeout(c,250)});c()})}})})(jQuery);
 
+// JQuery Notify by Antonio Villamarin <http://ant.onio.org> : MIT
+(function(e){
+	jQuery.fn.extend({
+		notify:function(options){
+			this.options = $.extend( {
+				show: true,
+				autoClose: 0,
+				close: true,
+				color: "#888",
+				background: "#FF9",
+				opacity: ".8",
+				message: "Message"
+			}, options);
+			
+			var active = this.options.show;
+
+			$('body').append('<div class="notify-message" style="display: none;"><span class="notify-close"></span><p>'+this.options.message+'</p></div><div class="notify-arrow" style="display: none;"></div>');
+			
+			$('.notify-arrow')
+				.css('background-color',this.options.background)
+				.css('z-index','101')
+				.css('position','absolute')
+				.css('top','0')
+				.css('right','50px')
+				.css('overflow','hidden')
+				.css('box-shadow','0 0 5px #444')
+				.css('opacity','0 0 5px #444')
+				.css('box-shadow',this.options.opacity)
+				.css('border-radius','0 0 5px 5px');
+				
+			$('.notify-arrow:before')
+				.css('diplay','block')
+				.css('padding','5px 15px')
+				.css('content','\25BC')
+				.css('color',this.options.color);
+
+			$('.notify-message')
+				.css('background-color',this.options.background)
+				.css('z-index','101')
+				.css('position','absolute')
+				.css('top','0')
+				.css('right','0')
+				.css('left','0')
+				.css('overflow','hidden')
+				.css('box-shadow','0 0 5px #444')
+				.css('opacity','0 0 5px #444')
+				.css('box-shadow',this.options.opacity)
+				.css('border-radius','0 0 5px 5px');
+				
+			$('.notify-message > p')
+				.css('margin','10px 20px')
+				.css('color',this.options.color)
+				.css('font-weight','bold');
+				
+			$('.notify-message > .notify-close:before')
+				.css('diplay','inline')
+				.css('float','right')
+				.css('margin','2px 15px 0 15px')
+				.css('content','\00D7')
+				.css('font-size','150%')
+				.css('font-weight','bold')
+				.css('color',this.options.color)
+
+		}
+	})
+})(jQuery);
+
 $(document).ready(function () {
 	$('textarea').elastic();
 	$('.tabs').tabify();
